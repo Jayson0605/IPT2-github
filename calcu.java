0f80f8aba2ac326ec1103d92adc72ba655d70108
Jayson0605 <jaysonbengson01@gmail.com>
2024-08-30 03:32:12 +0800

    Add method

--- calcu.java
+++ calcu.java
@@ -1,5 +1,10 @@
 public class calcu {
     public static void main(String[] args){
+        add(10, 10);
+    }
 
+    public static void add(int a, int b){
+        int sum = a + b;
+        System.out.println("The sum is: " +sum);
     }
 }

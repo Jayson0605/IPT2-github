f5bb6922aea6587c73a37b085a36f2cc158066b6
Jayson0605 <jaysonbengson01@gmail.com>
2024-08-30 03:47:42 +0800

    Add and sub method

--- calcu.java
+++ calcu.java
@@ -1,5 +1,16 @@
 public class calcu {
     public static void main(String[] args){
+        add(10, 10);
+        sub(10, 5);
+    }
 
+    public static void add(int a, int b){
+        int sum = a + b;
+        System.out.println("The sum is: " +sum);
+    }
+
+    public static void sub(int a, int b){
+        int res = a - b;
+        System.out.println("The difference is: " +res);
     }
 }
